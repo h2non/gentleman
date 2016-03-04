dbde855f5b036ad1190cbc4f627097d8004148d8
Tomas Aparicio <tomas@aparicio.me>
2016-03-04 08:58:09 +0100

    feat(example): addd inheritance example

---
+++ _examples/inheritance/inheritance.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/h2non/gentleman.v0"
+	"gopkg.in/h2non/gentleman.v0/plugins/headers"
+)
+
+func main() {
+	// Create a parent client
+	parent := gentleman.New()
+
+	// Define default URL
+	parent.URL("http://httpbin.org")
+
+	// Define a custom header via parent client
+	parent.Use(headers.Set("API-Token", "s3cr3t"))
+
+	// Create a new client
+	cli := gentleman.New()
+
+	// Bind parent client
+	cli.UseParent(parent)
+
+	// Create a new request based on the current client
+	req := cli.Request()
+
+	// Perform the request
+	res, err := req.Path("/post").JSON(map[string]string{"foo": "bar"}).Send()
+	if err != nil {
+		fmt.Printf("Request error: %s\n", err)
+		return
+	}
+	if !res.Ok {
+		fmt.Printf("Invalid server response: %d\n", res.StatusCode)
+		return
+	}
+
+	fmt.Printf("Status: %d\n", res.StatusCode)
+	fmt.Printf("Body: %s", res.String())
+}
